9e739bb09978211c55cc791482f1cdf705943a99
ShaakhDev <shohbobolov98@gmail.com>
2022-11-12 13:52:51 +0500

    initialize app

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+const Title = styled.Text`
+  padding: 20px;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover style={styles.cover} key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: { backGroundColor: "white" },
+  cover: { padding: 20, backgroundColor: "white" },
+});
